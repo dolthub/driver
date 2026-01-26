c3f989d54dd3c23d451b9e2b1dcc4a1b21086232
coffeegoddd☕️✨ <dustin@dolthub.com>
2026-01-26 10:15:03 -0800

    transaction.go: remove dead code

--- transaction.go
+++ transaction.go
@@ -2,22 +2,17 @@ package embedded
 
 import (
 	"database/sql/driver"
-	gms "github.com/dolthub/go-mysql-server/sql"
 )
 
 var _ driver.Tx = (*doltTx)(nil)
 
 type doltTx struct {
-	conn   *DoltConn
-	gmsCtx *gms.Context
+	conn *DoltConn
 }
 
 // Commit finishes the transaction.
 func (tx *doltTx) Commit() error {
 	se, gmsCtx := tx.conn.getEngineAndContext()
-	if tx.gmsCtx != nil {
-		gmsCtx = tx.gmsCtx
-	}
 	_, _, _, err := se.Query(gmsCtx, "COMMIT;")
 	if err == nil {
 		tx.conn.endTx()
@@ -28,9 +23,6 @@ func (tx *doltTx) Commit() error {
 // Rollback cancels the transaction.
 func (tx *doltTx) Rollback() error {
 	se, gmsCtx := tx.conn.getEngineAndContext()
-	if tx.gmsCtx != nil {
-		gmsCtx = tx.gmsCtx
-	}
 	_, _, _, err := se.Query(gmsCtx, "ROLLBACK;")
 	if err == nil {
 		tx.conn.endTx()
